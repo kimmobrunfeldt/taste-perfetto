a33a5aa86bc7c1502b235e943701b4dcc753c52e
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-10-13 01:25:41 +0300

    Site updated: 2015-10-13 01:25:40

--- js/index.js
+++ js/index.js
@@ -10,6 +10,26 @@
 
         $(".post-content").fitVids();
 
+        var $videos = $('video');
+        setInterval(function() {
+            var playingVideos = 0;
+
+            var inViewport = $videos.inViewport();
+            inViewport.each(function() {
+                if (playingVideos > 3) {
+                    this.pause();
+                    return;
+                }
+
+                this.play();
+                playingVideos++;
+            });
+
+            var notInViewport = $videos.not(inViewport);
+            notInViewport.each(function() {
+                this.pause();
+            });
+        }, 100);
     });
 
 }(jQuery));
